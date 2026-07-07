3813233db48eaf34408306ea0bd29c67d01ff50f
ERSING10 <ersingg77@gmail.com>
2026-07-07 18:37:23 +0300

    III-2

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,20 +1,22 @@
-import { View, useWindowDimensions } from 'react-native'
+import { View, ScrollView, useWindowDimensions } from 'react-native'
 import { Product } from '../types/product'
 import ProductCard from './ProductCard'
 
 type Props = {
-  products: Product[] // ürün dizisi, birden fazla ürün
+  products: Product[]
 }
 
 export default function ProductList({ products }: Props) {
-  const { width } = useWindowDimensions() // ekran genişliğini alır
-  const cardWidth = (width - 24) / 2 // 2 sütun, kenar boşlukları için 24 çıkardık
+  const { width } = useWindowDimensions()
+  const cardWidth = (width - 24) / 2
 
   return (
-    <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', padding: 8 }}>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} cardWidth={cardWidth} />
-      ))}
-    </View>
+    <ScrollView>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', padding: 8 }}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} cardWidth={cardWidth} />
+        ))}
+      </View>
+    </ScrollView>
   )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import { supabase } from '../lib/supabase'
+import { Product } from '../types/product'
+import ProductList from '../components/ProductList'
 
 export default function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Anasayfa</Text>
-    </View>
-  )
+
+    const[products, setProducts] = useState<Product[]>([]) // ürünleri tutan liste
+    const[loading, setLoading] = useState(true) // veri gelene kadar true
+
+    useEffect(() => {
+        fetchFeaturedProducts()
+    }, [])
+
+    async function fetchFeaturedProducts() {
+        const { data, error } = await supabase
+            .from('products')
+            .select('*')
+            .eq('is_featured',true) //sadece öne cıkanlar
+
+        if (error){
+            console.log('HATA:', error.message)
+        }
+        else
+            setProducts(data)
+
+        setLoading(false) // veri geldi || hata cıktı durumları icin false yapılır
+
+    }
+
+    if(loading){
+        return (
+            <View style={styles.centered}>
+                 <ActivityIndicator size="large" />
+            </View>
+        )
+    }
+
+    return <ProductList products={products} />
 }
+
+const styles = StyleSheet.create({
+  centered: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+})
